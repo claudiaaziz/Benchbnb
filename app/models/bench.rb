8fc36f1e969215ae7e12e8871befff47328e25ae
claudiaaziz <claudiahannahh@gmail.com>
2024-03-09 15:17:44 -0500

    Refactor bench-list CSS on BenchIndexPage

--- app/models/bench.rb
+++ app/models/bench.rb
@@ -1,6 +1,6 @@
 class Bench < ApplicationRecord
   validates :title, :description, :price, :seating, :lat, :lng, presence: true
-  validates :price, inclusion: { in: 10...1000, message: "must be between $10 and $1000" }
+  validates :price, inclusion: { in: 10..1000, message: "must be between $10 and $1000" }
 
   has_many :reviews, dependent: :destroy
 end
--- frontend/src/components/BenchIndexPage/BenchList.jsx
+++ frontend/src/components/BenchIndexPage/BenchList.jsx
@@ -4,10 +4,12 @@ import BenchListItem from './BenchListItem';
 const BenchList = ({ benches }) => {
 
   return (
-    <div className='bench-list'>
-      {Object.values(benches).map(bench => (
-        <BenchListItem bench={bench} key={bench.id}/>
-      ))}
+    <div className='bench-list-container'>
+      <div className='bench-list'>
+        {Object.values(benches).map(bench => (
+          <BenchListItem bench={bench} key={bench.id}/>
+        ))}
+      </div>
     </div>
   )
 }
