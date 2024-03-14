3790d2f1a947a20e903c429c616c2414df8b094c
claudiaaziz <claudiahannahh@gmail.com>
2024-03-14 18:25:32 -0400

    Work on profile button

--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import * as sessionActions from '../../store/session';
+import { openModal } from "../../store/modal";
 
-const ProfileButton = ({ user }) => {
+const ProfileButton = () => {
   const dispatch = useDispatch();
+  const sessionUser = useSelector(state => state.session.user);
   const [showMenu, setShowMenu] = useState(false);
   
   const openMenu = () => {
@@ -25,18 +27,28 @@ const ProfileButton = ({ user }) => {
     dispatch(sessionActions.logout());
   };
 
+  let sessionLinks;
+  if (sessionUser) {
+    sessionLinks = <button onClick={logout}>Log Out</button>
+  } else {
+    sessionLinks = (
+      <>
+        <button onClick={() => dispatch(openModal("login"))}>Log In</button>
+        <button onClick={() => dispatch(openModal("signup"))}>Sign Up</button>
+      </>
+    )
+  }
+
   return (
     <>
       <button className="profile-button" onClick={openMenu}>
-        <i className="fa-solid fa-user-circle" />
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" role="presentation" focusable="false" >
+          <path d="M16 .7C7.56.7.7 7.56.7 16S7.56 31.3 16 31.3 31.3 24.44 31.3 16 24.44.7 16 .7zm0 28c-4.02 0-7.6-1.88-9.93-4.81a12.43 12.43 0 0 1 6.45-4.4A6.5 6.5 0 0 1 9.5 14a6.5 6.5 0 0 1 13 0 6.51 6.51 0 0 1-3.02 5.5 12.42 12.42 0 0 1 6.45 4.4A12.67 12.67 0 0 1 16 28.7z"></path>
+        </svg>
       </button>
       {showMenu && (
         <ul className="profile-dropdown">
-          {/* <li>{user.username}</li> */}
-          {/* <li>{user.email}</li> */}
-          <li>
-            <button onClick={logout}>Log Out</button>
-          </li>
+          <li>{sessionLinks}</li>
         </ul>
       )}
     </>
--- frontend/src/components/Navigation/index.jsx
+++ frontend/src/components/Navigation/index.jsx
@@ -1,30 +1,13 @@
 import { NavLink } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
-import { openModal } from '../../store/modal';
 
 const Navigation = () => {
-  const dispatch = useDispatch()
-  const sessionUser = useSelector(state => state.session.user);
-
-  let sessionLinks;
-  if (sessionUser) {
-    sessionLinks = <ProfileButton user={sessionUser} />
-  } else {
-    sessionLinks = (
-      <>
-        <button onClick={() => dispatch(openModal("login"))}>Log In</button>
-        <button onClick={() => dispatch(openModal("signup"))}>Sign Up</button>
-      </>
-    )
-  }
-
   return (
     <>
       <div className='nav'>
         <NavLink exact to="/" className="logo">benchbnb</NavLink>
-        {sessionLinks}
+        <ProfileButton />
       </div>
       {/* <hr /> */}
     </>
